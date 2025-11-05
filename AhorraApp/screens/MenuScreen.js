e07e4e766877def56efbd09f2f7a17570ac370b3
124049707-dot <124049707@upq.edu.mx>
2025-11-05 03:38:44 -0600

    .

--- AhorraApp/screens/MenuScreen.js
+++ AhorraApp/screens/MenuScreen.js
@@ -8,25 +8,75 @@ import MainScreen from './MainScreen';
 import RegistroScreen from './RegistroScreen';
 import EditarTransaccionScreen from './EditarTransaccionScreen';
 import OlvidarContraScreen from './OlvidarContraScreen';
+import PresupuestoScreen from './PresupuestoScreen';
+import EditarPresuScreen from './EditarPresuScreen';
+import HomeScreen from './HomeScreen';
+import PagosProgramadosScreen from './PagosProgramadosScreen';
+import CrearPagoScreen from './CrearPagroScreen';
+import EditarPagoScreen from './EditarPagoScreen';
+import EstadisticasScreen from './EstadisticasScreen';
+import NotificationsScreen from './NotificacionesScreen';
+import PersonalizarScreen from './PersonalizarScreen';
+import MonedaScreen from './MonedaScreen';
+import PerfilScreen from './PerfilScreen';
+import EditarPerfilScreen from './EditarPerfilScreen';
+import AyudaScreen from './AyudaScreen';
+import IdiomaScreen from './IdiomaScreen';
+import SeguridadScreen from './SeguridadScreen.js';
+import CambiarContraScreen from './CambiarContraScreen.js';
+import ConfInicialScreen from './ConfInicialScreen.js';
+
 export default function MenuScreen() {
   const [screen, setScreen] = useState('menu');
   switch (screen) {
-    
     case 'transacciones':
-      return <TransaccionesScreen />; 
+      return <TransaccionesScreen />;
     case 'agregar':
-      return <AgregarTransaccionScreen />; 
+      return <AgregarTransaccionScreen />;
     case 'listado':
-      return <ListadoTransaccionesScreen />;  
+      return <ListadoTransaccionesScreen />;
     case 'editartrans':
-      return <EditarTransaccionScreen />;  
+      return <EditarTransaccionScreen />;
     case 'main':
-      return <MainScreen />;  
+      return <MainScreen />;
     case 'registro':
-      return <RegistroScreen />;  
+      return <RegistroScreen />;
+    case 'confinicial':
+      return <ConfInicialScreen />;
     case 'contra':
-      return <OlvidarContraScreen />; 
-
+      return <OlvidarContraScreen />;
+    case 'presupuesto':
+      return <PresupuestoScreen />;
+    case 'editarpresu':
+      return <EditarPresuScreen />;
+    case 'home':
+      return <HomeScreen />;
+    case 'pagosprogra':
+      return <PagosProgramadosScreen />;
+    case 'crearpago':
+      return <CrearPagoScreen />;
+    case 'editarpago':
+      return <EditarPagoScreen />;
+    case 'estadisticas':
+      return <EstadisticasScreen />;
+    case 'notificaciones':
+      return <NotificationsScreen />;
+    case 'moneda':
+      return <MonedaScreen />;
+    case 'perfil':
+      return <PerfilScreen />;
+    case 'editarperfil':
+      return <EditarPerfilScreen />;
+    case 'ayuda':
+      return <AyudaScreen />;
+    case 'personalizar':
+      return <PersonalizarScreen/>;
+    case 'idioma':
+      return <IdiomaScreen/>;
+    case 'seguridad':
+      return <SeguridadScreen/>;
+    case 'cambiarcontra':
+      return <CambiarContraScreen/>;
 
     case 'menu':
     default:
@@ -36,14 +86,172 @@ export default function MenuScreen() {
           <Text style={styles.subtitulo}>Menú Principal</Text>
 
           <View style={styles.botonera}>
-            <Button title="Pantalla Transacciones (Resumen)"onPress={() => setScreen('transacciones')}color="#4A90E2"/>
-            <Button title="Pantalla Agregar Transacción (Form)" onPress={() => setScreen('agregar')}color="#4A90E2"/>
-            <Button title="Pantalla Listado Transacciones" onPress={() => setScreen('listado')}color="#4A90E2"/>
-            <Button title="Pantalla Editar Transaccion" onPress={() => setScreen('editartrans')}color="#4A90E2"/>
-            <Button title="Pantalla Main" onPress={() => setScreen('main')}color="#4A90E2"/>
-            <Button title="Pantalla Registro" onPress={() => setScreen('registro')}color="#4A90E2"/>
-            <Button title="Pantalla Olvidar Contraseña" onPress={() => setScreen('contra')}color="#4A90E2"/>
-            
+            <Button
+              title="Pantalla Transacciones (Resumen)"
+              onPress={() => setScreen('transacciones')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Agregar Transacción (Form)"
+              onPress={() => setScreen('agregar')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Listado Transacciones"
+              onPress={() => setScreen('listado')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Editar Transaccion"
+              onPress={() => setScreen('editartrans')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Main"
+              onPress={() => setScreen('main')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Configuracion Inicial"
+              onPress={() => setScreen('confinicial')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Registro"
+              onPress={() => setScreen('registro')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Olvidar Contraseña"
+              onPress={() => setScreen('contra')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Presupuesto"
+              onPress={() => setScreen('presupuesto')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Editar Presupuesto"
+              onPress={() => setScreen('editarpresu')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Home"
+              onPress={() => setScreen('home')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Pagos Programados"
+              onPress={() => setScreen('pagosprogra')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Crear Pagos Programados"
+              onPress={() => setScreen('crearpago')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Editar Pagos Programados"
+              onPress={() => setScreen('editarpago')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Estadisticas"
+              onPress={() => setScreen('estadisticas')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Notificaciones"
+              onPress={() => setScreen('notificaciones')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Personalizar"
+              onPress={() => setScreen('personalizar')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Moneda"
+              onPress={() => setScreen('moneda')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Perfil"
+              onPress={() => setScreen('perfil')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Editar Perfil"
+              onPress={() => setScreen('editarperfil')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Ayuda"
+              onPress={() => setScreen('ayuda')}
+              color="#4A90E2"
+            />
+          </View>
+           <View style={styles.botonera}>
+            <Button
+              title="Pantalla Idioma"
+              onPress={() => setScreen('idioma')}
+              color="#4A90E2"
+            />
+          </View>
+          <View style={styles.botonera}>
+            <Button
+              title="Pantalla Seguridad"
+              onPress={() => setScreen('seguridad')}
+              color="#4A90E2"
+            />
+          </View>
+           <View style={styles.botonera}>
+            <Button
+              title="Pantalla Cambiar Contra"
+              onPress={() => setScreen('cambiarcontra')}
+              color="#4A90E2"
+            />
           </View>
         </ScrollView>
       );
@@ -72,9 +280,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   botonera: {
-    width: '80%', 
+    width: '80%',
     marginVertical: 8, 
-    borderRadius: 8, 
-    overflow: 'hidden', 
+    borderRadius: 8,
+    overflow: 'hidden',
   },
 });
