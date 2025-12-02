a38357833362c76532cf9ec83e64c7e3321bdcbc
Alex8aa <124051154@upq.edu.mx>
2025-12-01 20:00:57 -0600

    Update PersonalizarScreen.js

--- AhorraApp/screens/PersonalizarScreen.js
+++ AhorraApp/screens/PersonalizarScreen.js
@@ -4,7 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const LOGO_APP_IMAGE = require('../assets/recursos/Ahorro.png');
 
-//Función que le permite al usuario personalizar su configuracion
+//Funcion que le permite al usuario personalizar su configuracion
 export default function PersonalizarScreen({ navigation }) {
   const menuItems = [
     {
@@ -107,6 +107,7 @@ export default function PersonalizarScreen({ navigation }) {
   );
 }
 
+//Seccion de estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
